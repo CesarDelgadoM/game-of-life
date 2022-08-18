a969af1dd0aec71770b5c28a6f3e3596d8a0e772
cdelgado <cdelgado@informacolombia.com>
2022-08-17 21:36:26 -0500

    Finalizacion del proyecto

--- main.go
+++ main.go
@@ -1,59 +1,25 @@
 package main
 
 import (
-	"math/rand"
-	"time"
-
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
 const (
 	screenWidth  = 800
-	screenHeight = 600
+	screenHeight = 800
+	sizeCells    = 80
+	density      = 10
 )
-
-func init() {
-	rand.Seed(time.Now().UnixMilli())
-}
-
-type Game struct {
-	world *World
-	cells []Cell
-}
-
-func (g *Game) Layout(width int, height int) (int, int) {
-	return width, height
-}
-
-func (g *Game) Update() error {
-	return nil
-}
-
-func (g *Game) Draw(screen *ebiten.Image) {
-
-	// if g.pixels == nil {
-	// 	g.pixels = make([]byte, screenWidth*screenHeight*4)
-	// }
-
-	// g.world.Draw(g.pixels)
-	// screen.ReplacePixels(g.pixels)
-	g.cells[0].DrawCell(screen, 30, 30)
-}
 
 func main() {
 
-	cells := make([]Cell, screenWidth*screenHeight*4)
-	cells[0] = NewCell(4, 4)
-
-	game := &Game{
-		world: NewWorld(screenWidth, screenHeight, 10000),
-		cells: cells,
-	}
+	world := NewWorld(screenWidth, screenHeight, sizeCells, density)
+	world.GenerateLiveCells()
 
 	ebiten.SetWindowSize(screenWidth, screenHeight)
 	ebiten.SetWindowTitle("Game of Life")
 
-	if err := ebiten.RunGame(game); err != nil {
+	if err := ebiten.RunGame(world); err != nil {
 		panic(err)
 	}
 }
--- world.go
+++ world.go
@@ -1,49 +1,134 @@
 package main
 
-import "math/rand"
+import (
+	"image/color"
+	"math/rand"
+	"time"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
 
 type World struct {
-	area   []bool
-	width  int
-	height int
+	cells    [][]bool
+	sizeCell int
+	width    int
+	height   int
+	density  int
 }
 
-func NewWorld(width int, height int, maxLiveCells int) *World {
+func NewWorld(screenWidth int, screenHeight int, numberCells int, density int) *World {
 
-	world := &World{
-		area:   make([]bool, width*height),
-		width:  width,
-		height: height,
+	sizeCell := screenHeight / numberCells
+
+	cells := make([][]bool, screenHeight+sizeCell)
+	for i := range cells {
+		cells[i] = make([]bool, screenWidth+sizeCell)
 	}
-	world.init(maxLiveCells)
 
-	return world
-}
-
-func (w *World) init(maxLiveCells int) {
-
-	for i := 0; i < maxLiveCells; i++ {
-
-		x := rand.Intn(w.width)
-		y := rand.Intn(w.height)
-
-		w.area[y*w.width+x] = true
+	return &World{
+		cells:    cells,
+		width:    screenWidth,
+		height:   screenHeight,
+		sizeCell: sizeCell,
+		density:  density,
 	}
 }
 
-func (w *World) Draw(pixels []byte) {
+func (w *World) GenerateLiveCells() {
 
-	for i, value := range w.area {
-		if value {
-			pixels[4*i] = 0xff //255//color blanco
-			pixels[4*i+1] = 0xff
-			pixels[4*i+2] = 0xff
-			pixels[4*i+3] = 0xff
-		} else {
-			pixels[4*i] = 0
-			pixels[4*i+1] = 0
-			pixels[4*i+2] = 0
-			pixels[4*i+3] = 0
+	rand.Seed(time.Now().UnixNano())
+
+	for y := 0; y <= w.height; y += w.sizeCell {
+		for x := 0; x <= w.width; x += w.sizeCell {
+
+			if rand.Intn(w.density) == 1 {
+				w.cells[y][x] = true
+			}
+		}
+	}
+}
+
+func (w *World) Layout(width int, height int) (int, int) {
+	return width, height
+}
+
+func (w *World) Update() error {
+
+	w.executeRules()
+
+	return nil
+}
+
+func (w *World) executeRules() {
+
+	newCells := NewWorld(w.width, w.height, (w.height / w.sizeCell), w.density).cells
+
+	for y := 0; y <= w.height; y += w.sizeCell {
+		for x := 0; x <= w.width; x += w.sizeCell {
+
+			count := w.countNeighbours(x, y)
+			alive := w.cells[y][x]
+
+			if alive && count < 2 {
+				newCells[y][x] = false
+			}
+
+			if alive && (count == 2 || count == 3) {
+				newCells[y][x] = true
+			}
+
+			if alive && count > 3 {
+				newCells[y][x] = false
+			}
+
+			if !alive && count == 3 {
+				newCells[y][x] = true
+			}
+		}
+	}
+
+	w.cells = newCells
+}
+
+func (w *World) countNeighbours(x int, y int) int {
+
+	directions := []int{0, w.sizeCell, -w.sizeCell}
+	count := 0
+
+	for _, dx := range directions {
+		for _, dy := range directions {
+
+			if dx == 0 && dy == 0 {
+				continue
+			}
+
+			if w.isBounds(x+dx, y+dy) && w.cells[y+dy][x+dx] == true {
+				count++
+			}
+		}
+	}
+
+	return count
+}
+
+func (w *World) isBounds(x int, y int) bool {
+
+	xOK := x >= 0 && x < w.width+w.sizeCell
+	yOK := y >= 0 && y < w.height+w.sizeCell
+
+	return xOK && yOK
+}
+
+func (w *World) Draw(screen *ebiten.Image) {
+
+	for y := range w.cells {
+		for x := range w.cells[y] {
+
+			if w.cells[y][x] {
+				ebitenutil.DrawRect(screen, float64(x), float64(y),
+					float64(w.sizeCell), float64(w.sizeCell), color.White)
+			}
 		}
 	}
 }
